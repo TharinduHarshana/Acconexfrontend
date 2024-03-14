11b85f6a9de45625947c35d4080250dbe6797d69
RaveeshaSashini <106419788+raveeshasashini@users.noreply.github.com>
2024-03-14 12:57:32 +0530

    updateRoutes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-
 import React from 'react';
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserForm from './components/UserData/UserForm';
 import UserTable from './components/UserData/UserTable';
 import Dashboard from './components/Dashboard';
 import Login from './components/LoginComponents/Login';
 
-
-
-
 function App() {
-  const router=createBrowserRouter([
-    {
-      path:"/",
-      element:<Login></Login>
-    },{
-      path:"/userform",
-      element:<UserForm></UserForm>
-    },
-    {
-      path:"userTable",
-      element:<UserTable></UserTable>
-    },
-    {
-      path:"/dashboard",
-      element:<Dashboard></Dashboard>
-    }
-  ])
   return (
-    <RouterProvider router={router}></RouterProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/userform' element={<UserForm />} />
+        <Route path='/userTable' element={<UserTable />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

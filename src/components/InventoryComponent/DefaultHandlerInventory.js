54a2de0901e69c059cf3efe4e0f4e7353c9678bd
HirushaNAmarasinghe <127418195+HirushaNAmarasinghe@users.noreply.github.com>
2024-03-20 14:21:21 +0530

    dash update

--- src/components/InventoryComponent/DefaultHandlerInventory.js
+++ src/components/InventoryComponent/DefaultHandlerInventory.js
@@ -1,12 +1,13 @@
-import { Layout, theme } from "antd";
+import { Layout, theme,Menu } from "antd";
 import { useState } from "react";
-import {PercentageOutlined,DatabaseOutlined ,HomeOutlined,LogoutOutlined,UserAddOutlined ,ReconciliationOutlined,} from '@ant-design/icons';
+import {PercentageOutlined,DatabaseOutlined ,HomeOutlined,LogoutOutlined,SettingFilled,SyncOutlined} from '@ant-design/icons';
 import { Link } from "react-router-dom";
+import '../../styles/sidebar.css';
 
 
 const {Header, Sider, Content} = Layout
 
-const DefaultHandleInventory = (children) => {
+const DefaultHandleInventory = ({children}) => {
     const [collapsed] = useState(false);
     const[selectedKey, setSelectedKey] = useState(window.location.pathname);
 
@@ -19,32 +20,46 @@ const DefaultHandleInventory = (children) => {
     };
     return (
         <Layout>
-            Sider trigger={null} collapsible collapsed={collapsed}
+            <Sider trigger={null} collapsible collapsed={collapsed}>
             <div className="demo-logo-vertical"/>
-            <h3 className="text-center text-light text-weight-bold">ACONEX COMPUTER</h3>
-            <menu theme="dark"
+            <h3 className="AccTitle">ACONEX COMPUTER</h3>
+            <Menu theme="dark"
                 mode ="inline"
                 defaultSelectedKeys={[selectedKey]}
                 selectedKeys={selectedKey}
                 onClick={handleMenuClick}
                 >
-                <menu.item key = "/home" icon={<HomeOutlined/>}>
+                <Menu.Item key = "/home" icon={<HomeOutlined/>}>
                 <Link to ="/home">Home</Link>
-                </menu.item>
+                </Menu.Item>
 
-                <menu.item key = "/inventory" icon={<DatabaseOutlined/>}>
+                <Menu.Item key = "/inventory" icon={<DatabaseOutlined/>}>
                 <Link to ="/inventory">Inventory</Link>
-                </menu.item>
+                </Menu.Item>
 
-                <menu.item key = "/discountcalc" icon={<PercentageOutlined/>}>
+                <Menu.Item key = "/discountcalc" icon={<PercentageOutlined/>}>
                 <Link to ="/discountcalc">Discount</Link>
-                </menu.item>
-                <Menu.Item key="/log" icon={<LogoutOutlined  />}>
+                </Menu.Item>
+                <Menu.Item key="/log" icon={<LogoutOutlined/>}>
                 <Link to="/log">LOGOUT</Link>
                 </Menu.Item>
-                
-            </menu>
+            </Menu>
+            </Sider>
+            <Layout>
+            <Header style={{background: colorBgContainer}}>
+                <space className="topbar">
+                <HomeOutlined />
+                <SettingFilled />
+                <SyncOutlined spin />
+                <LogoutOutlined />
+                </space>
+            </Header>
+            <Content>
+                {children}
+            </Content>
         </Layout>
+        </Layout>
+    
     );
 }
  
--- src/components/InventoryComponent/InventoryDashboard.js
+++ src/components/InventoryComponent/InventoryDashboard.js
@@ -1,10 +1,94 @@
 import React from 'react'
 import DefaultHandleInventory from './DefaultHandlerInventory'
+import {Space, Typography, Card,Statistic} from 'antd'
+import{ShoppingCartOutlined,ShoppingOutlined, UserOutlined, DollarCircleOutlined,} from "@ant-design/icons";
+import '../../styles/sidebar.css';
+//import {Chart as ChartJS,CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend,} from "chart.js";
 
 function InventoryDashboard() {
   return (
-    <DefaultHandleInventory></DefaultHandleInventory>
+    <DefaultHandleInventory>
+      <div className='dashBoard'>
+        <Space size={20} direction='vertical'>
+        <Typography.Title level={4}>Dashboard</Typography.Title>
+        <Space direction='horizontal'>
+          <DashboardCard
+          icon ={
+            <ShoppingCartOutlined
+            style={{
+              color: "green",
+              backgroundColor: "rgba(0,255,0,0.25)",
+              borderRadius: 20,
+              fontSize: 24,
+              padding: 8,
+            }}
+          />
+        }
+        title={"Orders"}
+      
+      />
+      <DashboardCard
+        icon={
+          <ShoppingOutlined
+            style={{
+              color: "blue",
+              backgroundColor: "rgba(0,0,255,0.25)",
+              borderRadius: 20,
+              fontSize: 24,
+              padding: 8,
+            }}
+          />
+        }
+        title={"Inventory"}
+        
+      />
+      <DashboardCard
+        icon={
+          <UserOutlined
+            style={{
+              color: "purple",
+              backgroundColor: "rgba(0,255,255,0.25)",
+              borderRadius: 20,
+              fontSize: 24,
+              padding: 8,
+            }}
+          />
+        }
+        title={"Customer"}
+        
+      />
+      <DashboardCard
+        icon={
+          <DollarCircleOutlined
+            style={{
+              color: "red",
+              backgroundColor: "rgba(255,0,0,0.25)",
+              borderRadius: 20,
+              fontSize: 24,
+              padding: 8,
+            }}
+          />
+        }
+        title={"Revenue"}
+       
+      />
+        </Space>
+        </Space>
+
+      </div>
+    </DefaultHandleInventory>
+    
   )
+}
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
 }
 
 export default InventoryDashboard

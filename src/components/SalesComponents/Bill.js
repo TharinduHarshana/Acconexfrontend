2bb4c8a2a29eec56ba594427137df7d0766a2a4d
Hirunisha-Nirmani <hirunishanirmani@gmail.com>
2024-03-29 17:55:12 +0530

    bill
    bill change

--- src/components/SalesComponents/Bill.js
+++ src/components/SalesComponents/Bill.js
@@ -1,54 +1,50 @@
 import React from 'react';
-import { Input, Button, Flex, Table } from 'antd';
+import { Input, Button} from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import DefaultHandleSales from './DefaultHandleSales';
 import '../../styles/sales.css';
-
-const columns = [
-  {
-    title: 'Item_Code',
-    dataIndex: 'itemcode',
-    key: 'itemcode',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Item_Name',
-    dataIndex: 'itemname',
-    key: 'itemname',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Quantity',
-    dataIndex: 'Qnt',
-    key: 'Qnt',
-  },
-  {
-    title: 'Price',
-    dataIndex: 'itprice',
-    key: 'itprice',
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    itemcode: 'Item001',
-    itemname: 'Apple headset',
-    Qnt: 2,
-    itprice: 'RS: 1200/=',
-  },
-];
+import '../../styles/customer.css';
 
 const Bill = () => (
+  <div className='maindiv'>
   <DefaultHandleSales>
-    <Flex gap="small" vertical>
-      <Flex wrap="wrap" gap="small">
+    <div className='serchdiv'>
         <Input className='inboxserch' placeholder=' Enter the Item Name or Id' />
         <Button className='seachbtn' icon={<SearchOutlined />}>Search</Button>
-      </Flex>
-    </Flex>
-    <Table className ='table'columns={columns} dataSource={data} />
+        </div>
+        <div className="bill_container" >
+        <div className='bill_view'>
+          <div className='bill_tableContainer'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Item_ID</th>
+                  <th>Item_name</th>
+                  <th>Quantity</th>
+                  <th>Discount</th>
+                  <th></th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div  className='bill_FormContainer'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Item_ID</th>
+                  <th>Item_name</th>
+                  <th>Quantity</th>
+                  
+                </tr>
+              </thead>
+            </table>
+            </div>
+          </div>
+        </div>
+
+       
   </DefaultHandleSales>
+  </div>
 );
 
 export default Bill;
--- src/components/SalesComponents/SuspendSale.js
+++ src/components/SalesComponents/SuspendSale.js
@@ -1,58 +1,24 @@
 import React from 'react';
-import { Button, Table } from 'antd';
 import DefaultHandleSales from './DefaultHandleSales';
-import '../../styles/sales.css';
-
-const columns = [
-  {
-    title: 'Customer ID',
-    dataIndex: 'CusID',
-    key: 'CusID',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Customer Name',
-    dataIndex: 'cusname',
-    key: 'cusname',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Item ID',
-    dataIndex: 'ITID',
-    key: 'ITID',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Quantity',
-    dataIndex: 'Qnt',
-    key: 'Qnt',
-  },
-  {
-    title: 'Price',
-    dataIndex: 'itprice',
-    key: 'itprice',
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    CusID: 'Cus002',
-    cusname: 'Hiru',
-    ITID: 'Item001',
-    Qnt: 2,
-    itprice: 'RS: 1200/=',
-  },
-  // Add more data objects if needed
-];
+import '../../styles/customer.css';
 
 const SuspendSale = () => (
   <DefaultHandleSales>
-    
-    <Table className='table' columns={columns} dataSource={data} />
-    <div className="button-container">
-      <Button className="update-btn">Update</Button>
-      <Button className="ok-btn">Ok</Button>
+    <div className="container">
+      <div className='tableContainer'>
+        <table>
+          <thead>
+            <tr>
+              <th>Customer_ID</th>
+              <th>Customer_name</th>
+              <th>Suspend_ID</th>
+              <th>Bill_No</th>
+              <th>Total</th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   </DefaultHandleSales>
 );

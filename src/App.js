24e055ad13131699bde7dc019f7dd451f351fe39
TharinduHarshana <127109391+TharinduHarshana@users.noreply.github.com>
2024-04-26 09:36:14 +0530

    aaa

--- src/App.js
+++ src/App.js
@@ -1,27 +1,118 @@
-import React from 'react';
-import UserForm from './components/UserData/UserForm';
-import UserTable from './components/UserData/UserTable';
-import Login from './components/LoginComponents/Login';
-import { DatePicker } from 'antd';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import './styles/sidebar.css';
-import WebHomepage from './pages/WebHomepage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//login and dashboard import
+import Login from "./components/LoginComponents/Login";
+import Dashboard from "./components/SideBarComponent/DefaultHandle";
+import Homepage from "./pages/Homepage";
+
+//import user components
+import CreateUser from "./components/ManageUsers/CreateUser";
+import Users from "./components/ManageUsers/Users";
+import UpdateUser from "./components/ManageUsers/UpdateUser";
+
+//import customer and supplier components
+
+import CreateSupplierForm from "./components/ManageUsers/CreateSupplier";
+import Supplier from "./components/ManageUsers/Supplier";
+import UpdateSupplier from "./components/ManageUsers/UpdateSupplier";
+
+//import item kits
+import ItemKits from "./components/ManageInventory/ItemKits";
+import ItemKitsTable from "./components/ManageInventory/ItemKitsTable";
+
+//import sales components
+import SalesDachboad from './components/SalesComponents/SalesDachboad';
+import Bill from './components/SalesComponents/Bill';
+import Customer from './components/CustomerComponents/Customer';
+import SuspendSale from './components/SalesComponents/SuspendSale';
+
+import Daily_sales from './components/SalesComponents/Daily_sales';
+
+
+
+// web imports
+
+// import web components
+
+
+// import web components
+
+import WebHomepage from "./pages/WebHomepage";
+import CustomerForm from "./components/CustomerComponents/customerForm";
+
+//import inventory 
+import InventoryAdd from './components/InventoryComponent/Inventory.AddnewItem';
+import InventoryUpdate from './components/InventoryComponent/inventory.UpdateItem';
+import InventoryHome from './pages/Inventory';
+
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* User Routes */}
-        <Route path='/' element={<WebHomepage/>} />
+    {/* user routing path */}
+    <Route path="/" element={<WebHomepage />} />
+        
+
+       {/* admin routing path */}
+        <Route path="/admin" element={<Login/>} />
+        <Route path="/admin/dashbord" element={< Dashboard/>} />
+        <Route path="/admin/userform" element={<CreateUser />} />
+        <Route path="/admin/userform/update/:id" element={<UpdateUser/>} />
+        <Route path="/admin/userTable" element={<Users />} />
+        <Route path="/admin/customer" element={<Customer/>}/>
+
+       <Route path="/admin/customerform" element={<CustomerForm/>}/>
+
+
+
+        
+
+        <Route path="/admin/supplier" element={<Supplier/>}/>
+        <Route path="/admin/supplier/create" element={<CreateSupplierForm/>}/>
+        <Route path="/admin/supplier/update/:id" element={<UpdateSupplier />} />
+        <Route path="admin/inventory/item-kits" element={<ItemKits/>}/>
+        <Route path="admin/inventory/item-kits/table" element={<ItemKitsTable/>}/>
+
+
+
+        <Route path='sales' element={<SalesDachboad/>}/>
+        <Route path='bill' element={<Bill/>}/>
+        <Route path='addcus' element={<Customer/>}/>
+        <Route path='holdbill' element={<SuspendSale/>}/>
+        <Route path="dailysales" element={<Daily_sales/>}/>
+       
          
 
-        {/* admin routes */}
-        <Route path='/admin' element={<Login />} />
-        <Route path='/admin/userform' element={<UserForm />} />
-        <Route path='/admin/userTable' element={<UserTable />} />
-        <Route path="/admin/home" element={<Homepage />} />
+        <Route path='admin/sale' element={<SalesDachboad/>}/>
+        <Route path='admin/bill' element={<Bill/>}/>
+        <Route path='admin/addcus' element={<Customer/>}/>
+        <Route path='admin/holdbill' element={<SuspendSale/>}/>
+
+
+        {/* <Route path='admin/viewcustomer' element={<ViewCutomer/>}/> */}
+
+        <Route path='admin/inventory' element={<InventoryHome/>}/>
+        <Route path="admin/addnewitem" element={<InventoryAdd/>}/> 
+
+        
+        <Route path='/admin/sale' element={<SalesDachboad/>}/>
+        <Route path='/admin/bill' element={<Bill/>}/>
+        <Route path='/admin/addcus' element={<Customer/>}/>
+        <Route path='/admin/holdbill' element={<SuspendSale/>}/>
+        {/* <Route path='/admin/viewcustomer' element={<ViewCutomer/>}/> */}
+
+        <Route path='/admin/inventory' element={<InventoryHome/>}/>
+        <Route path="/admin/addnewitem" element={<InventoryAdd/>}/> 
+
+        <Route path="/admin/inventory/updateitem/:id" element={<InventoryUpdate/>}/>
+        
+        
+
+
       </Routes>
     </BrowserRouter>
   );
